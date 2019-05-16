a24cfc3c3fa6e09a304b5f0ffaeaf129dfe986e2
ArvindLoganathan <50648405+ArvindLoganathan@users.noreply.github.com>
2019-05-16 23:40:59 +0530

    Updates

---
+++ precache-manifest.984a1393c50d401bf4a720a8f91af41e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d8c7fe76ca1d7e08af30d469c56e32",
+    "url": "/First-React/index.html"
+  },
+  {
+    "revision": "b40e14f9e825b83239f2",
+    "url": "/First-React/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/First-React/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "b40e14f9e825b83239f2",
+    "url": "/First-React/static/js/main.7036c493.chunk.js"
+  },
+  {
+    "revision": "a9c3e50acc9041ba8c32",
+    "url": "/First-React/static/js/runtime~main.f37a3cde.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/First-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
